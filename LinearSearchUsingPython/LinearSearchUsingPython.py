05262f771dea6b3062bcad60c0de83bf94c3af00
Kartik Pawar <kartikmpawar.athani@hotmail.com>
2018-10-22 23:46:44 +0530

    Linear Search Using Python

--- LinearSearchUsingPython/LinearSearchUsingPython.py
+++ LinearSearchUsingPython/LinearSearchUsingPython.py
@@ -1,1 +1,28 @@
+def linearsearch(list,n):
+    found = 0
+    for i in range(len(list)):
+        if list[i]==n:
+            print("Element found at {} position".format(i+1))
+            found = 1
+            return True
+  
+    if found !=0:
+        print("Element {} not found".format(n))
+        return True
+    return
+
+list = []
+
+size = int(input("Enter the size of the array: "))
+
+for i in range(size):
+    x =  int(input("Enter the element at {} position: ".format(i+1)))
+    list.append(x)
+
+print("Entered array list is:")
+for lists in list:
+    print(lists,end="\t")
+
+se = int(input("\nEnter the array element to be searched: "))
+linearsearch(list,se)
 
